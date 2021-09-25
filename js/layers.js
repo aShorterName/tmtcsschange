d93bd002087476baa4a4857913765e1eb2a3b24c
kjkjdfkgj <luayip@gmail.com>
2021-09-26 07:47:22 +0800

    (init)

---
+++ js/layers.js
@@ -0,0 +1,21 @@
+addLayer("p", {//just read the docs...
+    name: "prestige",
+    symbol: "Main",
+    position: 1,
+    color: "#4BDC13",
+    type: "none",
+	startData() {
+		return {
+			vel: new Decimal(0)
+		}
+	},
+	tooltip: "",
+	update(dt) {
+		player.p.vel = player.p.vel.add(new Decimal(1).mul(dt))
+	},
+	tabFormat: [["raw-html", ()=>("You have gone a total of "+formatDistance(player.points)+ " ("+formatDistance(player.p.vel)+"/s)<br>Your current velocity is "+formatDistance(player.p.vel)+"/s (1 m/s)")]],
+    row: 0,
+    hotkeys: [
+    ],
+    layerShown(){return true}
+})
